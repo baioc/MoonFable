1a6d701f660e7def33a9be38c4c3346fa9141a04
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2020-03-22 20:07:42 -0300

    Animate board with coroutines

--- game/Puzzle.lua
+++ game/Puzzle.lua
@@ -5,23 +5,42 @@ function newBoard(rows, columns, x, y, stoneSize)
   setmetatable(self, Board)
   Board.__index = Board
 
+  -- board dimensions
   self.rows = rows
   self.columns = columns
-  self.x = x
-  self.y = y
-  self.stoneSize = stoneSize
 
   -- construct 2D board
   self.stones = {}
-  for i=1,self.rows do
+  for i = 1, self.rows do
     local row = {}
-    for j=1,self.columns do
+    for j = 1, self.columns do
       table.insert(row, newStone('random'))
     end
     table.insert(self.stones, row)
   end
 
+  -- constants used for drawing
+  self.x = x
+  self.y = y
+  self.stoneSize = stoneSize
+
+  -- animation control
+  self.locked = false
+  self.time = 0.0
+  self.animation = nil
+  self.timestep = 0.0
+
   return self
+end
+
+function Board:draw()
+  for i = 1, self.rows do
+    for j = 1, self.columns do
+      self.stones[i][j]:draw(self.x + (j - 1) * self.stoneSize,
+                             self.y + (i - 1) * self.stoneSize,
+                             self.stoneSize)
+    end
+  end
 end
 
 function Board:getStone(i, j) -- -> returns stone or nil if invalid indexes
@@ -32,12 +51,6 @@ function Board:getStone(i, j) -- -> returns stone or nil if invalid indexes
   end
 end
 
-function Board:swapStones(i, j, y, x)
-  self.stones[i][j], self.stones[y][x] = self.stones[y][x], self.stones[i][j]
-  self:checkMatch(self.stones[i][j]:getType(), self:absolutePosition(i, j))
-  self:checkMatch(self.stones[y][x]:getType(), self:absolutePosition(y, x))
-end
-
 -- computes a pair of indexes mapping to the stone under given mouse position
 function Board:getPositionUnder(x, y)
   local i = math.floor((y - self.y) / self.stoneSize) + 1
@@ -45,13 +58,99 @@ function Board:getPositionUnder(x, y)
   return i, j
 end
 
-function Board:draw()
-  for i=1,self.rows do
-    for j=1,self.columns do
-      self.stones[i][j]:draw(self.x + (j - 1) * self.stoneSize,
-                             self.y + (i - 1) * self.stoneSize,
-                             self.stoneSize)
+-- swaps stones and checks for any matches
+function Board:swapStones(i, j, y, x)
+  self.stones[i][j], self.stones[y][x] = self.stones[y][x], self.stones[i][j]
+  if not self.locked then
+    local matching = {}
+
+    local type1 = self.stones[i][j]:getType()
+    local n1 = self:checkMatch(type1, self:absolutePosition(i, j), matching)
+    local type2 = self.stones[y][x]:getType()
+    local n2 = self:checkMatch(type2, self:absolutePosition(y, x), matching)
+
+    if not n1 and not n2 then
+      return
     end
+
+    -- @TODO: what do we get by matching
+    if n1 then
+      print(type1 .. " -> " .. n1)
+    end
+    if n2 then
+      print(type2 .. " -> " .. n2)
+    end
+
+    -- collapse board accordingly
+    for _, stone in pairs(matching) do
+      stone:setColor(255, 255, 255)
+    end
+    self:animate(0.3, coroutine.create(function ()
+      for _, stone in pairs(matching) do
+        stone:setType('gap')
+      end
+      local moved = true
+      repeat
+        coroutine.yield()
+        moved = false
+        for i = self.rows,1 , -1 do -- bottom-up
+          for j = 1, self.columns do
+            if self.stones[i][j]:getType() == 'gap' then
+              if i == 1 then
+                self.stones[i][j]:setType('random')
+              elseif self.stones[i-1][j]:getType() ~= 'gap' then
+                self:swapStones(i, j, i-1, j)
+              end
+              moved = true
+            end
+          end
+        end
+      until not moved
+    end))
+  end
+end
+
+function Board:checkMatch(type, origin, matching)
+  -- breadth-first search algorithm
+  local visited = {}
+  visited[origin] = true
+  local queue = {origin}
+
+  local function visit (i, j)
+    if j < 1 or j > self.columns or i < 1 or i > self.rows then
+      return -- skip invalid positions
+    end
+    local position = self:absolutePosition(i, j)
+    if visited[position] then
+      return -- skip already-visited positions
+    end
+    -- mark as visited and add it to the queue if it has the right type
+    visited[position] = true
+    if self.stones[i][j]:getType() == type then
+      table.insert(queue, position)
+    end
+  end
+
+  local match = {}
+  local n = 0
+  repeat
+    local position = table.remove(queue, 1)
+    local i, j = self:matrixIndexes(position)
+    match[position] = self.stones[i][j]
+    n = n + 1
+                    visit(i - 1, j)
+    visit(i, j - 1)                 visit(i, j + 1)
+                    visit(i + 1, j) -- try doing this in python
+  until #queue < 1
+
+  -- must match 3 or more
+  if n >= 3 then
+    for k, v in pairs(match) do
+      matching[k] = v
+    end
+    return n
+  else
+    return nil
   end
 end
 
@@ -65,58 +164,31 @@ function Board:matrixIndexes(position)
   return i, j
 end
 
-function Board:checkMatch(type, origin)
-  -- gaps shouldn't match
-  if type == 'gap' then return end
+function Board:isLocked()
+  return self.locked
+end
 
-  local visited = {}
-  visited[origin] = true
-  local queue = {origin}
-
-  local function visit (i, j)
-    if j < 1 or j > self.columns or i < 1 or i > self.rows then
-      return -- skip invalid positions
-    end
-
-    local position = self:absolutePosition(i, j)
-    if visited[position] then
-      return -- skip already-visited positions
-    end
-
-    -- mark as visited and add it to the queue if it has the right type
-    visited[position] = true
-    if self.stones[i][j]:getType() == type then
-      table.insert(queue, position)
-    end
-  end
-
-  local matching = {}
-  local n = 0
-  repeat
-    local position = table.remove(queue, 1)
-    local i, j = self:matrixIndexes(position)
-    matching[position] = self.stones[i][j]
-    n = n + 1
-    -- try doing this in python
-                    visit(i - 1, j)
-    visit(i, j - 1)                 visit(i, j + 1)
-                    visit(i + 1, j)
-  until #queue < 1
-
-  -- collapse stones if match 3 or more
-  if n >= 3 then
-    -- @FIXME: missing an actual animation
-    -- @TODO: what do we get by matching
-    for _, stone in pairs(matching) do
-      -- stone:setType('gap')
-      stone:setColor(nil, nil, nil, 0.25)
+function Board:update(dt)
+  if self.locked then
+    self.time = self.time + dt
+    if self.time >= self.timestep then
+      self.time = self.time - self.timestep
+      if not coroutine.resume(self.animation) then
+        self.locked = false
+      end
     end
   end
 end
 
+function Board:animate(dt, co)
+  self.locked = true
+  self.animation = co
+  self.timestep = dt
+end
+
 
 Stone = {
-  types = {'attack', 'defend', 'magic', 'potion'},
+  types = {'attack', 'heal', 'magic', 'defend'},
 }
 
 function newStone(type)
@@ -130,6 +202,22 @@ function newStone(type)
   return self
 end
 
+function Stone:draw(x, y, size)
+  love.graphics.setColor(self.color.r, self.color.g, self.color.b, self.color.a)
+  love.graphics.rectangle('fill', x, y, size, size)
+end
+
+function Stone:getColor()
+  return self.color.r, self.color.g, self.color.b, self.color.a
+end
+
+function Stone:setColor(r, g, b, ...) -- optional alpha value
+  self.color.r = r or self.color.r
+  self.color.g = g or self.color.g
+  self.color.b = b or self.color.b
+  self.color.a = select(1, ...) or self.color.a
+end
+
 function Stone:getType()
   return self.type
 end
@@ -139,7 +227,7 @@ function Stone:setType(type)
     self.color.r = 255
     self.color.g = 0
     self.color.b = 0
-  elseif type == 'defend' then
+  elseif type == 'heal' then
     self.color.r = 0
     self.color.g = 255
     self.color.b = 0
@@ -147,7 +235,7 @@ function Stone:setType(type)
     self.color.r = 0
     self.color.g = 0
     self.color.b = 255
-  elseif type == 'potion' then
+  elseif type == 'defend' then
     self.color.r = 255
     self.color.g = 255
     self.color.b = 0
@@ -161,21 +249,4 @@ function Stone:setType(type)
     error("invalid Stone type " .. tostring(type))
   end
   self.type = type
-end
-
-function Stone:getColor()
-  return self.color.r, self.color.g, self.color.b, self.color.a
-end
-
-function Stone:setColor(r, g, b, ...) -- optional: alpha value
-  self.color.r = r or self.color.r
-  self.color.g = g or self.color.g
-  self.color.b = b or self.color.b
-  local arg = {...}
-  self.color.a = arg[1] or self.color.a
-end
-
-function Stone:draw(x, y, size)
-  love.graphics.setColor(self.color.r, self.color.g, self.color.b, self.color.a)
-  love.graphics.rectangle('fill', x, y, size, size)
 end
--- main.lua
+++ main.lua
@@ -1,17 +1,29 @@
 require 'game/Puzzle'
 
 
-local CURSOR_ARROW = love.mouse.getSystemCursor('arrow')
-local CURSOR_HAND = love.mouse.getSystemCursor('hand')
+-- constants
+local CURSOR_NORMAL = nil
+local CURSOR_MOVE = nil
 
+-- global state
 local board = nil
-local hovered = {stone = nil}
-local grabbed = {stone = nil, i = nil, j = nil}
+local hovered = nil
+local grabbed = nil
 
 
 function love.load()
   math.randomseed(os.time())
+
+  CURSOR_NORMAL = love.mouse.getSystemCursor('arrow')
+  CURSOR_MOVE = love.mouse.getSystemCursor('sizeall')
+
   board = newBoard(8, 9, 36, 352, 64)
+  hovered = {stone = nil}
+  grabbed = {stone = nil, i = nil, j = nil}
+end
+
+function love.update(dt)
+  board:update(dt)
 end
 
 function love.draw()
@@ -19,6 +31,7 @@ function love.draw()
 end
 
 function love.mousemoved(x, y, dx, dy, istouch)
+  -- get stone under the mouse (if any)
   local i, j = board:getPositionUnder(x, y)
   local stone = board:getStone(i, j)
 
@@ -26,11 +39,11 @@ function love.mousemoved(x, y, dx, dy, istouch)
   if love.mouse.isDown(1) and grabbed.stone and stone and grabbed.stone ~= stone then
     -- stones must have a Manhattan distance of 1
     if math.abs(grabbed.i - i) + math.abs(grabbed.j - j) == 1 then
-      board:swapStones(i, j, grabbed.i, grabbed.j)
-      stone = grabbed.stone
+      board:swapStones(i, j, grabbed.i, grabbed.j) -- may lock the board
+      stone = board:getStone(i, j)
     end
     grabbed.stone = nil
-    love.mouse.setCursor(CURSOR_ARROW)
+    love.mouse.setCursor(CURSOR_NORMAL)
   end
 
   -- "unhover" the previously hovered stone
@@ -40,24 +53,24 @@ function love.mousemoved(x, y, dx, dy, istouch)
   end
 
   -- if there's a stone there, apply hover effect
-  if stone then
+  if stone and not board:isLocked() then
     stone:setColor(nil, nil, nil, 0.75)
     hovered.stone = stone
   -- otherwise out of board, so release grabbed stone (if any)
   elseif grabbed.stone then
     grabbed.stone = nil
-    love.mouse.setCursor(CURSOR_ARROW)
+    love.mouse.setCursor(CURSOR_NORMAL)
   end
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-  -- if left button is pressed, grab the stone under the mouse
-  if button == 1 then
+  -- if left button is pressed, grab the stone under the mouse (if any)
+  if button == 1 and not board:isLocked() then
     local i, j = board:getPositionUnder(x, y)
     grabbed.stone = board:getStone(i, j)
     if grabbed.stone then
       grabbed.i, grabbed.j = i, j
-      love.mouse.setCursor(CURSOR_HAND)
+      love.mouse.setCursor(CURSOR_MOVE)
     end
   end
 end
@@ -66,6 +79,6 @@ function love.mousereleased(x, y, button, istouch, presses)
   -- as left button is released, release grabbed stone (if any)
   if button == 1 then
     grabbed.stone = nil
-    love.mouse.setCursor(CURSOR_ARROW)
+    love.mouse.setCursor(CURSOR_NORMAL)
   end
 end
